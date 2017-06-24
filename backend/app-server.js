f2c637c4f35b2d0362775d98bc72d4d7e1095d68
Reginald Mathieu <reginald.w.mathieu@gmail.com>
2017-06-24 19:17:32 -0400

    update

--- backend/app-server.js
+++ backend/app-server.js
@@ -74,7 +74,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     var allowedOrigins = ['http://localhost:3000', 'http://localhost:9000', 'https://straight-fire.herokuapp.com', 'http://www.straight-fire.com', 'http://straight-fire.com'];
-    var origin = req.headers.origin || (req.headers['x-forwarded-proto'] + '//' + req.headers.host);
+    var origin = req.headers.origin || (req.headers['x-forwarded-proto'] + '://' + req.headers.host);
 
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', '*');
